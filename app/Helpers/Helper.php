aaf9886e8b790838358fa6e8c1f720fbbc6076a9
ibrahim-devwork <105915674+ibrahim-devwork@users.noreply.github.com>
2022-10-05 17:06:14 +0100

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -10,11 +10,15 @@ class  Helper {
     const count_per_page = 15;
 
     // Static functions
-    public static function saveFile($image, $folder_name) {
+    public static function saveFile($image, $folder_name = '') {
         if(isset($image) && is_file($image)) {
             $imageName  = now()->format('Ymd-His-u-') . Str::random(3);
             $imageName  = $imageName .'.'. $image->getClientOriginalExtension();
-            $destinationPath = public_path('images/'. $folder_name .'/');
+            if($folder_name) {
+                $destinationPath = public_path('images/'. $folder_name .'/');
+            } else {
+                $destinationPath = public_path('images/');
+            }
             $image->move($destinationPath, $imageName);
             return $imageName;
         }
